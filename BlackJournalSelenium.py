5a72ac18d5bd421f520e18ecdf208ced3372c6e0
ZacharySayers <118835583+ZacharySayers@users.noreply.github.com>
2023-07-13 12:31:45 -0400

    attribute finder works

--- BlackJournalSelenium.py
+++ BlackJournalSelenium.py
@@ -1,23 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# link to website
 website = 'https://fixitplus.americanarchive.org/transcripts/cpb-aacip_62-7d2q52fm1v'
 
 # path not needed anymore due to selenium update
 # path = r"C:\Users\zsaye\Downloads\chromedriver_win32\chromedriver.exe")
 
+# needed so website doesn't immediately close
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
 driver.get(website)
 
-search_page = driver.find_elements(By.XPATH, './/*[@id="transcript-lines"]/div')
+search_page = driver.find_elements(By.TAG_NAME, 'input')
 
 for transcript in search_page:
-    print(transcript.getAttribute("value"))
+    print(transcript.get_attribute('value'))
+
 
 # the only input to give back some values
+
 '''
 transcript = driver.find_elements(By.TAG_NAME, "input")
 
