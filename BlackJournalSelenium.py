cae8a100757721ff51411a5749d7fa46d428ece4
ZacharySayers <118835583+ZacharySayers@users.noreply.github.com>
2023-07-14 01:04:06 -0400

    cleaned up code

--- BlackJournalSelenium.py
+++ BlackJournalSelenium.py
@@ -4,29 +4,20 @@ from selenium.webdriver.common.by import By
 # link to website
 website = 'https://fixitplus.americanarchive.org/transcripts/cpb-aacip_62-7d2q52fm1v'
 
-# path not needed anymore due to selenium update
-# path = r"C:\Users\zsaye\Downloads\chromedriver_win32\chromedriver.exe")
-
-# needed so website doesn't immediately close
+# chrome driver
 options = webdriver.ChromeOptions()
+# needed so website doesn't immediately close
 options.add_experimental_option("detach", True)
 
+# retrieves website using driver
 driver = webdriver.Chrome(options=options)
 driver.get(website)
 
+# selenium iterates through given parameters for info
 search_page = driver.find_elements(By.TAG_NAME, 'input')
 
 for transcript in search_page:
     print(transcript.get_attribute('value'))
 
-
-# the only input to give back some values
-
-'''
-transcript = driver.find_elements(By.TAG_NAME, "input")
-
-#for phrases in transcript:
-    #print(phrases)
-'''
 
 
